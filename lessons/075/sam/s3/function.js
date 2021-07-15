f1624c4c216ccd250e5eef650c701215326c6aff
Anton Putra <me@antonputra.com>
2021-07-14 20:29:08 -0700

    tmp

--- lessons/075/sam/s3/function.js
+++ lessons/075/sam/s3/function.js
@@ -3,15 +3,13 @@ const aws = require('aws-sdk');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 exports.lambdaHandler = async (event, context) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-
     // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     const params = {
         Bucket: bucket,
         Key: key,
-    }; 
+    };
     try {
         const { ContentType } = await s3.getObject(params).promise();
         console.log('CONTENT TYPE:', ContentType);
